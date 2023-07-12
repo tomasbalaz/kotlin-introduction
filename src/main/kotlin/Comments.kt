a9fe91065b72815f1f669fb2f074df95d8b4e656
Tomas Balaz <balaztomas0@gmail.com>
2023-07-12 15:33:04 +0200

    Examples for comments

---
+++ src/main/kotlin/Comments.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    //one line comment
+    // println("Hello please comment me")
+
+    //multi line comment
+    /*
+    adadadsa
+    dasdasdsad
+    asdasdas
+    println("Multi line)
+     */
+}

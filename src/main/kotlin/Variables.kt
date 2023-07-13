dfe7a73c309d3a99726d5f494a772c0d989f4818
Tomas Balaz <balaztomas0@gmail.com>
2023-07-13 15:19:57 +0200

    Examples for using variables

---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    var name: String = "Jamal"
+    var age: Int = 18
+
+    //1st option of print value
+    print(name)
+    print(age)
+
+    //2nd option of print value
+    print("Name: ${name} Age: ${age}")
+
+}

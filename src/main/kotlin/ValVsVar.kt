180d657a0ddde34f43de0a9f5176158903834f12
Tomas Balaz <balaztomas0@gmail.com>
2023-07-13 15:46:34 +0200

    Example of difference for Kotlin var vs val

---
+++ src/main/kotlin/ValVsVar.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    var brandVar = "var example"
+    brandVar = "Can be changed"
+
+    val brandVal = "val example"
+    // Java concept example
+    // final String brand = "Can not be changed"
+
+// val can not be changed, it is muttable
+    //brandVal = "Can not be changed"
+
+}

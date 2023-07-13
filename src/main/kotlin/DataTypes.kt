3ed73ebb0a9c1951013fee8516ed21d658402166
Tomas Balaz <balaztomas0@gmail.com>
2023-07-13 15:30:50 +0200

    Example for data types

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    //Java syntax
+    // int number = 19;
+
+    //Kotlin syntax
+    var number: Int = 10
+
+    var l: Long = 1024L
+
+    var d: Double = 3.14
+
+    var f: Float = 3.14F
+
+    var b: Boolean = true
+
+    var s: String = "string"
+
+    var c: Char = 'a'
+}

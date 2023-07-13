f6c799b95d54efa3083e91103d8641be3340d240
Tomas Balaz <balaztomas0@gmail.com>
2023-07-13 15:40:42 +0200

    Example for data Kotlin Type Inference

---
+++ src/main/kotlin/TypeInference.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    //Kotlin Type Inference
+    var number = 10
+
+    var l = 1024L
+
+    var d = 3.14
+
+    var f = 3.14F
+
+    var b = true
+
+    var s = "string"
+
+    var c = 'a'
+}

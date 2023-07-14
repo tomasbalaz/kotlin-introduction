c9d447a4a755d9bbb9923a0afb30cdfdeedada94
Tomas Balaz <balaztomas0@gmail.com>
2023-07-14 15:44:00 +0200

    Example of Constants

---
+++ src/main/kotlin/Const.kt
@@ -0,0 +1,22 @@
+
+// 'Pi' has been known before code execution - difference between val PI = 3.14
+// -> static final -> Java Example
+const val PI = 3.14
+
+
+// !!!! Const 'val' initializer should be a constant value
+//const val PI = getPiValue()
+
+val pi = getPiValue()
+
+fun main() {
+
+    val name = "Jamal" // value -> read only
+    // final -> Java example
+   // name = ""
+
+    var i = 0 // muttable
+    i = 2
+}
+
+fun getPiValue(): Double = 3.14

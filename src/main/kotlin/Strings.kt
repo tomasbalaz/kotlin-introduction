5052a48f8ac3a0a636086510992ecca4c847bddb
Tomas Balaz <balaztomas0@gmail.com>
2023-07-17 15:46:46 +0200

    Example of Strings templates

--- src/main/kotlin/Strings.kt
+++ src/main/kotlin/Strings.kt
@@ -6,4 +6,24 @@ fun main() {
     println(name.length)
     println(name[0])
     println("".isEmpty())
+
+    val age = 20
+
+    //1st option of the message concatenation
+    //val msg = name + " " + age
+
+    //2nd option of the message concatenation
+    //val msg = "${name} ${age}"
+
+    //3th option of the message concatenation
+   // val msg = "Name $name,  Age $age"
+
+    //4th option of the message concatenation
+    //val msg = "Name $name,  Age ${age + 1}"
+
+    //5th option of the message concatenation
+    val msg = "Name ${name.uppercase()},  Age ${age + 1}"
+
+
+    println(msg)
 }

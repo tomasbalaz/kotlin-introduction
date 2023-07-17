49756f878a41274c380bde7d28e020baf436c04b
Tomas Balaz <balaztomas0@gmail.com>
2023-07-17 15:36:54 +0200

    Example of Strings

---
+++ src/main/kotlin/Strings.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val name = "Tomas"
+    println(name)
+    println(name.uppercase())
+    println(name.lowercase())
+    println(name.length)
+    println(name[0])
+    println("".isEmpty())
+}

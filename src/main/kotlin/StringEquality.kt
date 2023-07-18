6333fe07e6be05d16982ec8dd7c736c37f430fa0
Tomas Balaz <balaztomas0@gmail.com>
2023-07-18 11:34:53 +0200

    Example of String equality

---
+++ src/main/kotlin/StringEquality.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val name1 = "Tomas"
+
+    val name2 = "Jamila"
+
+    val name3 = "Tomas"
+
+    val name4 = String("Saleh".toCharArray())
+
+    // ==  and 'equals method' equals 'content' of the String
+
+    println(name1 == name3)
+    println("== ${name1 == name3}")
+
+    println(name1.equals(name3))
+    println(".equals ${name1.equals(name3)}")
+
+    println("=== ${name1 === name4}")
+
+}

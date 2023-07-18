056cc6cc4a167321aac11e876692759b235d884c
Tomas Balaz <balaztomas0@gmail.com>
2023-07-18 14:22:28 +0200

    Example of increment and decrement operator

---
+++ src/main/kotlin/IncDecOperators.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    // ++ and --
+
+    //val number = 10
+    var number = 10
+
+    println(++number) // 11
+    println(number++) // 11
+    println(number) // 12
+
+    println(--number) // 11
+    println(number--) // 11
+    println(number) // 10
+}

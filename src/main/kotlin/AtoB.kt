29940bf3e33c46e3b69ccb025b647233fea2d7f1
Tomas Balaz <balaztomas0@gmail.com>
2023-07-18 14:57:20 +0200

    Example of how to convert one data type to another

---
+++ src/main/kotlin/AtoB.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val number = "10"
+    println(10 + number.toInt())
+
+    //println(10.toString() + number)
+    println("10$number")
+
+    val d= 3.0
+    println(d)
+    println(d.toInt())
+}

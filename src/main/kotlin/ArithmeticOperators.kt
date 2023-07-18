58debce0391da0bb61b24b981c92bf6fda2d62ce
Tomas Balaz <balaztomas0@gmail.com>
2023-07-18 11:46:40 +0200

    Example of Kotlin arithmetic operators

---
+++ src/main/kotlin/ArithmeticOperators.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+    val number1 = 10
+
+    val number2 = 2
+
+    println(number1 + number2)
+    println(number1 - number2)
+    println(number1 * number2)
+    println(number1 / number2)
+    println(number1 % number2)
+
+    println("+ ${number1 + number2}")
+    println("- ${number1 - number2}")
+    println("* ${number1 * number2}")
+    println("/ ${number1 / number2}")
+    println("% ${number1 % number2}")
+
+}

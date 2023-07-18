fe9e7f8f40b300d98c39eafc4f18a5a8e2c4bcae
Tomas Balaz <balaztomas0@gmail.com>
2023-07-18 11:55:40 +0200

    Example of math functions

---
+++ src/main/kotlin/TheMath.kt
@@ -0,0 +1,16 @@
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.round
+import kotlin.math.sqrt
+
+fun main() {
+    val number1 = 10
+
+    val number2 = 2
+
+    println(kotlin.math.PI)
+    println(max(1,2))
+    println(min(1,2))
+    println(sqrt(5.0))
+    println(round(4.2))
+}

73d53203b3a971acada4fd250db9553f05ed322a
Tomas Balaz <balaztomas0@gmail.com>
2023-07-19 15:21:19 +0200

    Example of Boolean usage

---
+++ src/main/kotlin/conditionals/Booleans.kt
@@ -0,0 +1,12 @@
+package conditionals
+
+fun main() {
+
+    val isAdult = true
+    val isMale = false
+    val isOrderCompleted: Boolean? = null
+
+    println(isAdult)
+    println(isMale)
+    println(isOrderCompleted)
+}

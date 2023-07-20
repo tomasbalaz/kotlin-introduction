e80df62df3e65856a3df44bc94316e2078761168
Tomas Balaz <balaztomas0@gmail.com>
2023-07-20 15:28:46 +0200

    Example haw to use if expression

---
+++ src/main/kotlin/conditionals/IfExpression.kt
@@ -0,0 +1,16 @@
+package conditionals
+
+fun main() {
+
+    val n1 = 10
+
+    val n2 = 20
+
+    val value = if(n1 > n2) {
+        ":)"
+    } else {
+        ":("
+    }
+
+    println(value)
+}

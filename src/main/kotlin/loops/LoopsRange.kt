81c0147b5c96cfc57bfe4965503fa40236a4ed42
Tomas Balaz <balaztomas0@gmail.com>
2023-07-26 14:59:44 +0200

    Basic usage of loops range

---
+++ src/main/kotlin/loops/LoopsRange.kt
@@ -0,0 +1,15 @@
+package loops
+
+fun main() {
+
+    println("range")
+    for (i in 1..5) println(i)
+    println("downTo")
+    for (i in 5 downTo 1) println(i)
+
+    println("step")
+    for (i in 1..5 step 2) println(i)
+    println("step downTo")
+    for (i in 5 downTo 1 step 2) println(i)
+
+}

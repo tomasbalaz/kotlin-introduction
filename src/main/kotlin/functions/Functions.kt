beddb881b8ca1503b8ed279597933f05ef763b85
Tomas Balaz <balaztomas0@gmail.com>
2023-07-27 15:22:18 +0200

    Basic usage of functions as arguments

--- src/main/kotlin/functions/Functions.kt
+++ src/main/kotlin/functions/Functions.kt
@@ -5,6 +5,22 @@ fun main() {
     greet(age = 23, name = "Tomas")
     greet("Alex")
     greet("Bob", 11)
+    foo(bar = {
+        println("bar as a function")
+
+    })
+    foo {
+        println("bar as a function 2")
+    }
+    foo()
+}
+
+fun foo(
+    name: String = "",
+    bar: () -> Unit = {}) {
+
+    println("bar function")
+    bar()
 }
 
 fun greet(name: String, age: Int = -1) {

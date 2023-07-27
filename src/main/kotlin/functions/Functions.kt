a81a6fe3703a219ee100e0cddff2f14abd27b291
Tomas Balaz <balaztomas0@gmail.com>
2023-07-27 15:12:27 +0200

    Basic usage of default arguments

--- src/main/kotlin/functions/Functions.kt
+++ src/main/kotlin/functions/Functions.kt
@@ -3,16 +3,20 @@ package functions
 fun main() {
     println("hello".uppercase())
     greet(age = 23, name = "Tomas")
-    greet("Alex", 16)
+    greet("Alex")
     greet("Bob", 11)
 }
 
-fun greet(name: String, age: Int) {
+fun greet(name: String, age: Int = -1) {
     println("Hello $name")
+    if(age == -1) {
+        println("Age not provided")
+    }
     if(age >= 16) {
         println("$name is an adult")
     }
     else {
         println("$name not an adult")
     }
+    println()
 }

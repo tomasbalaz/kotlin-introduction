52bb8f6ec87c0c45459681322da2529dbaed4641
Tomas Balaz <balaztomas0@gmail.com>
2023-07-31 14:34:57 +0200

    Basic usage of property classes

--- src/main/kotlin/classesAndObjects/Classes.kt
+++ src/main/kotlin/classesAndObjects/Classes.kt
@@ -2,11 +2,19 @@ package classesAndObjects
 
 fun main() {
     val tv = SmartDevice()
+    tv.brand = "Samsung"
+    tv.price = 10000.0
+
     val phone = SmartDevice()
+    phone.brand = "Apple"
+    phone.price = 1200.00
 }
 
 // blueprint for objects
 class SmartDevice {
     //properties variables val or var
+    var brand: String = ""
+
+    var price: Double = 0.0
     //behaviours functions
 }

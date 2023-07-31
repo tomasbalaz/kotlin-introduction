2c785068f3276e8600654b2dccf6b421050326c1
Tomas Balaz <balaztomas0@gmail.com>
2023-07-31 14:25:10 +0200

    Basic usage of classes

---
+++ src/main/kotlin/classesAndObjects/Classes.kt
@@ -0,0 +1,12 @@
+package classesAndObjects
+
+fun main() {
+    val tv = SmartDevice()
+    val phone = SmartDevice()
+}
+
+// blueprint for objects
+class SmartDevice {
+    //properties variables val or var
+    //behaviours functions
+}

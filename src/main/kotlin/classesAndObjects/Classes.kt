ddadc53e1c4b768821b7b31bf0aaf8242f310f5b
Tomas Balaz <balaztomas0@gmail.com>
2023-07-31 14:43:53 +0200

    Basic usage of getters and setters in classes

--- src/main/kotlin/classesAndObjects/Classes.kt
+++ src/main/kotlin/classesAndObjects/Classes.kt
@@ -8,13 +8,22 @@ fun main() {
     val phone = SmartDevice()
     phone.brand = "Apple"
     phone.price = 1200.00
+
+    println("${tv.brand} ${tv.price}")
+    println("${phone.brand} ${phone.price}")
 }
 
 // blueprint for objects
 class SmartDevice {
     //properties variables val or var
     var brand: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
 
     var price: Double = 0.0
-    //behaviours functions
+        //behaviours functions
+        get() = field
+
 }
